f172c2aeb39d1f77f2ab3c00fca3f3c1e62a9457
imyogeshgaur <yogeshgaur1995@gmail.com>
2024-02-13 10:41:52 +0530

    Added Resuable components

---
+++ src/Components/TextInput.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import "../css/AddTodo.css";
+
+const TextInput = (props) => {
+  return (
+    <>
+      <div className="label-container">
+        <label htmlFor={props.name}>{props.label}</label>
+        <input type={props.type} name={props.name} onChange={props.onChange} value={props.value} />
+      </div>
+    </>
+  )
+}
+
+export default TextInput
