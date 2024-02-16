759f9d3b5037a6d3c5ee59a675023d4cb0f5906b
imyogeshgaur <yogeshgaur1995@gmail.com>
2024-02-16 11:00:01 +0530

    Added Link to Sign Up Page

--- src/pages/Auth/Login.jsx
+++ src/pages/Auth/Login.jsx
@@ -1,13 +1,14 @@
 import { useState } from 'react'
 import "../../css/Login.css"
-import { collection, query, where, getDocs, limit } from "firebase/firestore"
+import { collection, query, where, getDocs } from "firebase/firestore"
 import { isEmptyData, isValidEmail } from '../../utils/Validate'
 import db from '../../config/Firebase'
 import { AuthInput } from '../../Components/Tags/TextInput'
 import { useNavigate } from 'react-router'
+import { Link } from 'react-router-dom'
 
 const Login = () => {
-  const navigate= useNavigate();
+  const navigate = useNavigate();
   const [user, setUser] = useState({
     emailOrUserName: "",
     passwordOfUser: ""
@@ -25,16 +26,23 @@ const Login = () => {
     try {
       if (!isEmptyData(user.emailOrUserName) && !isEmptyData(user.passwordOfUser)) {
         if (isValidEmail(user.emailOrUserName)) {
-          const loginQuery = query(collection(db, "users"), where("emailOfUser", "==", user.emailOrUserName),where("passwordOfUser","==",user.passwordOfUser))
+          const loginQuery = query(collection(db, "users"), where("emailOfUser", "==", user.emailOrUserName), where("passwordOfUser", "==", user.passwordOfUser))
           const loginSnapShot = await getDocs(loginQuery);
 
-          if(loginSnapShot.docs.length!==0){
-              navigate("/home")
-          }else{
+          if (loginSnapShot.docs.length !== 0) {
+            navigate("/home")
+          } else {
             console.log("Invalid Credentials !!!")
           }
         } else {
+          const loginQuery = query(collection(db, "users"), where("userNameOfUser", "==", user.emailOrUserName), where("passwordOfUser", "==", user.passwordOfUser))
+          const loginSnapShot = await getDocs(loginQuery);
 
+          if (loginSnapShot.docs.length !== 0) {
+            navigate("/home")
+          } else {
+            console.log("Invalid Credentials !!!")
+          }
         }
       } else {
         console.log("Please fill all data")
@@ -50,6 +58,23 @@ const Login = () => {
         <AuthInput name={"emailOrUserName"} type={"email"} label={"Email or User Name"} onChange={onChangeHandler} value={user.emailOrUserName} />
         <AuthInput name={"passwordOfUser"} type={"password"} label={"Password"} onChange={onChangeHandler} value={user.passwordOfUser} />
         <button onClick={LoginUser}>Login</button>
+        <p
+          style={{ marginLeft: "8rem" }}
+        >
+          New to our site? &nbsp;
+          <Link
+            to="/register"
+            style={{ textDecoration: "none", color: "#3a30ca" }}
+          >
+            SignUp Here
+          </Link>
+        </p>
+        <Link
+          to="/forgetPassword"
+          style={{ textDecoration: "none", color: "#3a30ca", marginLeft: "21em" }}
+        >
+          Forget Password
+        </Link>
       </div>
     </>
   )
