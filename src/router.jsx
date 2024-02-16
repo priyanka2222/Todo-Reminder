bede09440d53108635d983ea7e2c2bd8402db41b
imyogeshgaur <yogeshgaur1995@gmail.com>
2024-02-16 10:59:19 +0530

    Added Forget Password Route

--- src/router.jsx
+++ src/router.jsx
@@ -2,15 +2,15 @@ import { lazy } from "react";
 
 const SignUpPage = lazy(() => import("./pages/Auth/SignUp"))
 const LoginPage = lazy(() => import("./pages/Auth/Login"))
+const ForgetPassword = lazy(() => import("./pages/Auth/ForgetPassword"))
 const DisplayTodosPage = lazy(() => import("./pages/Todos/DisplayTodos"))
-// const EditTodoPage = lazy(() => import("./pages/Todos/EditTodo"))
 const AddTodoPage = lazy(() => import("./pages/Todos/AddTodo"))
 
 const routers = [
     { path: "/", element: <LoginPage /> },
     { path: "/register", element: <SignUpPage /> },
+    { path: "/forgetPassword", element: <ForgetPassword /> },
     { path: "/home", element: <DisplayTodosPage /> },
-    // { path: "/edit/:id", element: <EditTodoPage /> },
     { path: "/addTodo", element: <AddTodoPage /> }
 ]
 
