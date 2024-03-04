f5648e3ef4a13dc9802998cfc4d45fa015fe8836
imyogeshgaur <yogeshgaur1995@gmail.com>
2024-03-04 11:04:12 +0530

    Make NavBar dynamic

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,13 +1,30 @@
 import React from 'react'
 import "../css/NavBar.css"
-
-const Navbar = () => {
+import { useLocation } from 'react-router'
+import {useNavigate} from "react-router-dom"
+const Navbar = (props) => {
+    const location = useLocation()
+    const navigate = useNavigate()
+    const addTodo = () => {
+        navigate("/addTodo")
+    }
+    const goBack = () => {
+        window.history.back();
+    }
+    const logout = ()=>{
+        localStorage.removeItem("userName");
+        navigate("/")
+    }
     return (
         <>
-            <div className='display-bar'>
-                <h1 style={{marginTop:10}}>Todo</h1>
-                <button >Add</button>
-                <button >Logout</button>
+            <div className={location.pathname === "/home" ? 'display-bar' : 'display-bar-home'}>
+                <h1 style={{ marginTop: 10 }}>{props.title}</h1>
+                <button 
+                onClick={
+                    location.pathname === "/home" ? 
+                    addTodo : goBack
+                }>{props.btn1}</button>
+                <button onClick={logout}>{props.btn2}</button>
             </div>
         </>
     )
